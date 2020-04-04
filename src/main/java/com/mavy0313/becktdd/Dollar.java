8414cb154958e2fc425ade11d0af849b6a88189b
mavy0313 <vypoff@gmail.com>
2020-04-04 22:40:27 +0400

    First commit

---
+++ src/main/java/com/mavy0313/becktdd/Dollar.java
@@ -0,0 +1,20 @@
+package com.mavy0313.becktdd;
+
+class Dollar {
+
+  int amount;
+
+  Dollar(int amount) {
+    this.amount = amount;
+  }
+
+  Dollar times(int multiplier) {
+    return new Dollar(amount * multiplier);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    Dollar dollar = (Dollar) obj;
+    return amount == dollar.amount;
+  }
+}
---
+++ src/test/java/com/mavy0313/becktdd/MoneyTest.java
@@ -0,0 +1,25 @@
+package com.mavy0313.becktdd;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+  @Test
+  public void multiplication() {
+    Dollar five = new Dollar(5);
+    Dollar product = five.times(2);
+    assertEquals(10, product.amount);
+    product = five.times(3);
+    assertEquals(15, product.amount);
+  }
+
+  @Test
+  public void equality() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
+  }
+}
