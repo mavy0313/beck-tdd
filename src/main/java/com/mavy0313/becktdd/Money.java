c8a18c90a93449d6f05a679b4038e4e3762088b0
mavy0313 <vypoff@gmail.com>
2020-04-16 02:08:44 +0400

    Mixed addition

--- src/main/java/com/mavy0313/becktdd/Money.java
+++ src/main/java/com/mavy0313/becktdd/Money.java
@@ -24,7 +24,7 @@ class Money implements Expression {
         && currency().equals(money.currency());
   }
 
-  Money times(int multiplier) {
+  Expression times(int multiplier) {
     return new Money(amount * multiplier, currency);
   }
 
@@ -37,7 +37,7 @@ class Money implements Expression {
     return amount + " " + currency;
   }
 
-  Expression plus(Money addend) {
+  public Expression plus(Expression addend) {
     return new Sum(this, addend);
   }
 
